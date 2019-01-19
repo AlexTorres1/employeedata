91d290d3c3b1656abcb1b4aaef618b4035cbf952
Daniel Moreno <daniel.moreno94@me.com>
2019-01-19 11:31:43 -0500

    JS push

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyD6dw1xWLSW3FQ-9zY06ALa9Oe9arhMd6A",
+    authDomain: "employee-data-a8df6.firebaseapp.com",
+    databaseURL: "https://employee-data-a8df6.firebaseio.com",
+    projectId: "employee-data-a8df6",
+    storageBucket: "employee-data-a8df6.appspot.com",
+    messagingSenderId: "1008780745263"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
